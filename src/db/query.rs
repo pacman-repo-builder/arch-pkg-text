f703758b8c2dd56615c0403d3426e4111b55ff01
khai96_ <hvksmr1996@gmail.com>
2025-01-28 01:18:40 +0700

    style: fix format

--- src/db/query.rs
+++ src/db/query.rs
@@ -35,22 +35,22 @@ macro_rules! def_traits {
 }
 
 def_traits! {
-    file_name, file_name_mut  = FileName -> FileName;
-    name, name_mut  = Name -> Name;
-    base, base_mut  = Base -> Base;
-    version, version_mut  = Version -> Version;
-    description, description_mut  = Description -> Description;
+    file_name, file_name_mut = FileName -> FileName;
+    name, name_mut = Name -> Name;
+    base, base_mut = Base -> Base;
+    version, version_mut = Version -> Version;
+    description, description_mut = Description -> Description;
     groups, groups_mut = Groups -> GroupList;
-    compressed_size, compressed_size_mut  = CompressedSize -> Size;
-    installed_size, installed_size_mut  = InstalledSize -> Size;
-    md5_checksum, md5_checksum_mut  = Md5Checksum -> Hex128;
-    sha256_checksum, sha256_checksum_mut  = Sha256Checksum -> Hex256;
-    pgp_signature, pgp_signature_mut  = PgpSignature -> PgpSignature;
-    url, url_mut  = Url -> Url;
+    compressed_size, compressed_size_mut = CompressedSize -> Size;
+    installed_size, installed_size_mut = InstalledSize -> Size;
+    md5_checksum, md5_checksum_mut = Md5Checksum -> Hex128;
+    sha256_checksum, sha256_checksum_mut = Sha256Checksum -> Hex256;
+    pgp_signature, pgp_signature_mut = PgpSignature -> PgpSignature;
+    url, url_mut = Url -> Url;
     license, license_mut = License -> LicenseList;
     architecture, architecture_mut = Architecture -> ArchitectureList;
-    build_date, build_date_mut  = BuildDate -> Timestamp;
-    packager, packager_mut  = Packager -> Packager;
+    build_date, build_date_mut = BuildDate -> Timestamp;
+    packager, packager_mut = Packager -> Packager;
     dependencies, dependencies_mut = Dependencies -> DependencyList;
     make_dependencies, make_dependencies_mut = MakeDependencies -> DependencyList;
     check_dependencies, check_dependencies_mut = CheckDependencies -> DependencyList;
