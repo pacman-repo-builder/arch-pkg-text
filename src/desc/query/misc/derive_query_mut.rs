2433c31dbffe79dde39106ef0d63c5b6b1b45e0b
khai96_ <hvksmr1996@gmail.com>
2025-02-18 02:05:00 +0700

    refactor: move deeper

---
+++ src/desc/query/misc/derive_query_mut.rs
@@ -0,0 +1,21 @@
+use crate::desc::{EncourageReuse, ParsedField, Query, QueryMut};
+
+/// Wrapper struct to permit [`QueryMut`] on a struct that only implements [`Query`].
+#[derive(Debug, Default, Clone, Copy)]
+pub struct DeriveQueryMut<Querier: ?Sized>(pub Querier);
+
+impl<'a, Querier: Query<'a> + ?Sized> Query<'a> for DeriveQueryMut<Querier> {
+    fn query_raw_text(&self, field: ParsedField) -> Option<&'a str> {
+        self.0.query_raw_text(field)
+    }
+}
+
+impl<'a, Querier: Query<'a> + ?Sized> QueryMut<'a> for DeriveQueryMut<Querier> {
+    fn query_raw_text_mut(&mut self, field: ParsedField) -> Option<&'a str> {
+        self.query_raw_text(field)
+    }
+}
+
+impl<'a, Querier: Query<'a> + EncourageReuse + ?Sized> EncourageReuse for DeriveQueryMut<Querier> {
+    const ENCOURAGE_REUSE: bool = Querier::ENCOURAGE_REUSE;
+}
--- src/srcinfo/query/misc.rs
+++ src/srcinfo/query/misc.rs
@@ -1,27 +1,5 @@
 //! Miscellaneous items.
 
-use super::{EncourageReuse, Query, QueryMut, QueryRawTextItem};
-use crate::srcinfo::FieldName;
+mod derive_query_mut;
 
-/// Wrapper struct to permit [`QueryMut`] on a struct that only implements [`Query`].
-#[derive(Debug, Default, Clone, Copy)]
-pub struct DeriveQueryMut<Querier: ?Sized>(pub Querier);
-
-impl<'a, Querier: Query<'a> + ?Sized> Query<'a> for DeriveQueryMut<Querier> {
-    fn query_raw_text(&self, field_name: FieldName) -> impl Iterator<Item = QueryRawTextItem<'a>> {
-        self.0.query_raw_text(field_name)
-    }
-}
-
-impl<'a, Querier: Query<'a> + ?Sized> QueryMut<'a> for DeriveQueryMut<Querier> {
-    fn query_raw_text_mut(
-        &mut self,
-        field_name: FieldName,
-    ) -> impl Iterator<Item = QueryRawTextItem<'a>> {
-        self.query_raw_text(field_name)
-    }
-}
-
-impl<'a, Querier: Query<'a> + EncourageReuse + ?Sized> EncourageReuse for DeriveQueryMut<Querier> {
-    const ENCOURAGE_REUSE: bool = Querier::ENCOURAGE_REUSE;
-}
+pub use derive_query_mut::*;
