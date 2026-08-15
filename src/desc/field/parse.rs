58619b0f8cc83c47740024f2bba9cd23686e769e
Khải <hvksmr1996@gmail.com>
2026-08-15 18:08:09 +0700

    docs: remove a line of docs
    Co-authored-by: Khải <hvksmr1996@gmail.com>

--- src/desc/field/parse.rs
+++ src/desc/field/parse.rs
@@ -51,7 +51,6 @@ pub enum ParseRawFieldError {
     IncorrectEndingCharacter,
     #[display("Field name is empty")]
     Empty,
-    /// The index is relative to the field name, which is the input without the surrounding `%`.
     #[display("Found invalid character {_1:?} at index {_0}")]
     InvalidCharacter(usize, char),
 }
